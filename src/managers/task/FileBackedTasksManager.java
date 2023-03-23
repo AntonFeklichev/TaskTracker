4881849b54cdfa3efd6f3cdc7b6721ef6e910db5
Anton <afeklichev@yandex.ru>
2023-03-23 19:20:04 +0300

    финал стпринт 6

--- src/managers/task/FileBackedTasksManager.java
+++ src/managers/task/FileBackedTasksManager.java
@@ -4,20 +4,14 @@ import com.google.gson.Gson;
 import entity.Epic;
 import entity.SubTask;
 import entity.Task;
+import exceptions.ManagerSaveException;
 
 import java.io.*;
 import java.util.List;
 
 public class FileBackedTasksManager extends InMemoryTaskManager {
 
-    //FileWriter fileWriter = new FileWriter("Task.csv");
 
-    /* public static void main(String[] args) throws IOException {
-        FileWriter fileWriter = new FileWriter("Task.csv");
-        fileWriter.write("Hello");
-        Gson gson = new Gson();
-
-    } */
 
 
     private final String fileName;
@@ -33,17 +27,16 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
             String json = gson.toJson(this);
             writer.write(json);
 
-            //writer.close();
+
 
         } catch (IOException exception) {
-            System.out.println("Не удалось записать файл");
+            throw new ManagerSaveException("Файл не сохранен");
 
         }
     }
 
     public static FileBackedTasksManager loadFromFile(String fileName) {
         try (Reader reader = new FileReader(fileName)) {
-            //reader.read(fileName);
             Gson gson = new Gson();
             return gson.fromJson(reader, FileBackedTasksManager.class);
 
@@ -61,7 +54,7 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
     }
 
     @Override
-    public void addEpic(Epic epic){
+    public void addEpic(Epic epic) {
         super.addEpic(epic);
         save();
     }
@@ -72,8 +65,83 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         save();
     }
 
+    @Override
+    public void clearAllTask() {
+        super.clearAllTask();
+        save();
+    }
 
+    @Override
+    public void clearAllEpic() {
+        super.clearAllEpic();
+        save();
+    }
 
+    @Override
+    public void clearAllSubTask() {
+        super.clearAllSubTask();
+        save();
+    }
+
+    @Override
+    public Task getTask(int id) {
+        Task task = super.getTask(id);
+        save();
+        return task;
+    }
+
+    @Override
+    public Epic getEpic(int id) {
+        Epic epic = super.getEpic(id);
+        save();
+        return epic;
+    }
+
+    @Override
+    public SubTask getSubTask(int id) {
+        SubTask subTask = super.getSubTask(id);
+        save();
+        return subTask;
+    }
+
+    @Override
+    public Boolean updateTask(Task task) {
+        Boolean updateTask = super.updateTask(task);
+        save();
+        return updateTask;
+    }
+
+    @Override
+    public Boolean updateEpic(Epic epic) {
+        Boolean updateEpic = super.updateEpic(epic);
+        save();
+        return updateEpic;
+    }
+
+    @Override
+    public Boolean updateSubTask(SubTask subTask) {
+        Boolean updateSubTask = super.updateSubTask(subTask);
+        save();
+        return updateSubTask;
+    }
+
+    @Override
+    public void removeTask(Task task) {
+        super.removeTask(task);
+        save();
+    }
+
+    @Override
+    public void removeEpic(Epic epic) {
+        super.removeEpic(epic);
+        save();
+    }
+
+    @Override
+    public void removeSubTask(SubTask subTask) {
+        super.removeSubTask(subTask);
+        save();
+    }
 }
 
 
